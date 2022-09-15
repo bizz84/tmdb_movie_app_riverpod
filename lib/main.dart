30d23090914b907b4c48566ece4ae41e990d0089
Andrea Bizzotto <codewithandrea@gmail.com>
2022-09-15 10:10:23 +0100

    Replace flutter_dotenv with envied

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tmdb_movie_app_riverpod/src/presentation/movies_search_page.dart';
 
 Future<void> main() async {
-  await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: MyApp()));
 }
 
--- lib/src/data/movies_repository.dart
+++ lib/src/data/movies_repository.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tmdb_movie_app_riverpod/env/env.dart';
 import 'package:tmdb_movie_app_riverpod/src/data/dio_provider.dart';
 import 'package:tmdb_movie_app_riverpod/src/domain/tmdb_movie_basic.dart';
 import 'package:tmdb_movie_app_riverpod/src/domain/tmdb_movies_response.dart';
@@ -49,7 +49,7 @@ class MoviesRepository {
 final fetchMoviesRepositoryProvider = Provider<MoviesRepository>((ref) {
   return MoviesRepository(
     client: ref.watch(dioProvider),
-    apiKey: dotenv.env['TMDB_KEY']!,
+    apiKey: Env.tmdbApiKey,
   );
 });
 
