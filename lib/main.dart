c64c8d7a3e2d5b0aadc4c334b953a674526ccf17
Andrea Bizzotto <codewithandrea@gmail.com>
2022-09-21 16:28:54 +0100

    Add go router nested routes (#2)
    * Freezed classes no longer need to be abstract
    
    * Add GoRouter with bottom navigation
    
    * Cleanup
    
    * More cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tmdb_movie_app_riverpod/src/presentation/movies_search_page.dart';
+import 'package:tmdb_movie_app_riverpod/src/routing/app_router.dart';
 
 Future<void> main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final goRouter = ref.watch(goRouterProvider);
+    return MaterialApp.router(
+      routerConfig: goRouter,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: const MoviesSearchPage(),
     );
   }
 }
