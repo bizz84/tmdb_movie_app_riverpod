9bfbfe2ab84e26698b337b7dd7637e0271be6911
Andrea Bizzotto <codewithandrea@gmail.com>
2023-03-06 16:48:10 +0000

    Updated generated files

--- lib/src/features/movies/domain/tmdb_movies_response.freezed.dart
+++ lib/src/features/movies/domain/tmdb_movies_response.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of core;
 
@@ -170,6 +170,7 @@ class _$_TMDBMoviesResponse implements _TMDBMoviesResponse {
   final List<TMDBMovie> _results;
   @override
   List<TMDBMovie> get results {
+    if (_results is EqualUnmodifiableListView) return _results;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_results);
   }
@@ -184,6 +185,7 @@ class _$_TMDBMoviesResponse implements _TMDBMoviesResponse {
   @override
   @JsonKey()
   List<String> get errors {
+    if (_errors is EqualUnmodifiableListView) return _errors;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_errors);
   }
