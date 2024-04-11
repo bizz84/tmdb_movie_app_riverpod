5cc8ffa2eb1502f97bed05b471672c6bc17c1cc3
Andrea Bizzotto <codewithandrea@gmail.com>
2024-04-11 11:02:55 +0100

    Improved interceptor

--- lib/src/utils/logger_interceptor.dart
+++ lib/src/utils/logger_interceptor.dart
@@ -3,27 +3,62 @@ import 'dart:developer';
 import 'package:dio/dio.dart';
 
 class LoggerInterceptor implements Interceptor {
-  @override
-  void onError(DioException err, ErrorInterceptorHandler handler) {
-    log('❌ Dio Error!');
-    log('❌ Url: ${err.requestOptions.uri}');
-    log('❌ ${err.stackTrace}');
-    log('❌ Response Error: ${err.response?.data}');
-    return handler.next(err);
-  }
+  final stopwatches = <String, Stopwatch>{};
 
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    log('🌍 Sending network request: ${options.baseUrl}${options.path}');
+    final url = '${options.baseUrl}${options.path}';
+    stopwatches[url] = Stopwatch()..start();
+    log('🌍 Making request: $url');
     return handler.next(options);
   }
 
   @override
   void onResponse(Response response, ResponseInterceptorHandler handler) {
-    log('⬅️ Received network response');
-    log('${response.statusCode != 200 ? '❌ ${response.statusCode} ❌' : '✅ 200 ✅'} ${response.requestOptions.baseUrl}${response.requestOptions.path}');
-    log('Query params: ${response.requestOptions.queryParameters}');
+    final url =
+        '${response.requestOptions.baseUrl}${response.requestOptions.path}';
+    _logMessageAndClearStopwatch(
+        response.statusCode, url, '⬅️ Received response');
+    if (response.requestOptions.queryParameters.isNotEmpty) {
+      log('Query params: ${response.requestOptions.queryParameters}');
+    }
     log('-------------------------');
     return handler.next(response);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    final url = err.requestOptions.uri.toString();
+    _logMessageAndClearStopwatch(null, url, '❌ Received error');
+    log('${err.stackTrace}');
+    if (err.response?.data != null) {
+      log('❌ Response Error: ${err.response?.data}');
+    }
+    return handler.next(err);
+  }
+
+  void _logMessageAndClearStopwatch(
+      int? statusCode, String url, String message) {
+    final stopwatch = stopwatches[url];
+    if (stopwatch != null) {
+      stopwatch.stop();
+      _logResponse(statusCode, stopwatch.elapsedMilliseconds, url);
+      stopwatches.remove(url);
+    } else {
+      log(message);
+    }
+  }
+
+  void _logResponse(int? statusCode, int milliseconds, String url) {
+    final emoji = switch (statusCode) {
+      != null && >= 200 && < 300 => '✅',
+      != null && >= 300 && < 400 => '🟠',
+      _ => '❌'
+    };
+    if (statusCode != null) {
+      log('$emoji $statusCode $emoji | ${milliseconds}ms | $url');
+    } else {
+      log('$emoji | ${milliseconds}ms | $url');
+    }
   }
 }
